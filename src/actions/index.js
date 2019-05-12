3dcaf1454df053361a7767b79b40d7bae1a16b42
Rustam Khikmatullin <rustam.hikmatullin@piano.io>
2019-05-12 14:44:34 +0400

    Created action functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,35 @@ export const ADD_TODO = 'ADD_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const EDIT_TODO = 'EDIT_TODO';
+
+let nextId = 5;
+
+export function addTodo(title) {
+    return {
+        type: ADD_TODO,
+        id: nextId++,
+        title: title
+    }
+};
+
+export function deleteTodo(id) {
+    return {
+        type: DELETE_TODO,
+        id: id
+    }
+};
+
+export function toggleTodo(id) {
+    return {
+        type: TOGGLE_TODO,
+        id: id
+    }
+};
+
+export function editTodo(id, title) {
+    return {
+        type: EDIT_TODO,
+        id: id,
+        title: title
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore } from 'redux';
+import reducer from './reducers';
+import {addTodo, editTodo, toggleTodo, deleteTodo} from './actions';
 
 import todos from './todos';
 import App from './App';
+
+const store = createStore(reducer, todos);
 
 ReactDOM.render(<App initialData={todos} />, document.getElementById('root'));
